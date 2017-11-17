3b0c60dd980f43f722e16abbdca2d93a6478df7b
Azarus29 <pierre-antoyne.fontaine@laposte.net>
2017-11-17 11:46:09 +0100

    Add controls (only play, pause/resume, stop working

---
+++ app/scripts/directives/controls.js
@@ -0,0 +1,57 @@
+'use strict';
+
+angular.module('testApp')
+	.directive('controls', function (fileService,playService) {
+		return {
+			templateUrl: 'views/controls.html',
+			restrict: 'E',
+			scope: {},
+			link: function postLink(scope,element){
+
+				scope.zoomIn = function(){
+					console.log("zoomIn");
+					//if start / stop !== undefined
+					//then change them
+				};
+
+				scope.zoomOut = function(){
+					console.log("zoomOut");
+					//if start / stop !== undefined
+					//change them
+				};
+
+				scope.toLeft = function(){
+					console.log("toLeft");
+					//if start / stop !== undefined
+					//change them
+				};
+
+				scope.toRight = function(){
+					console.log("toRight");
+					//if start / stop !== undefined
+					//change them
+				};		
+
+				scope.play = function(){
+					console.log("play");
+					//if audioBuffer !== undefined
+					//play it
+					if(fileService.getAudioBuffer()!==undefined){
+						playService.playFromTo(0,fileService.getAudioBuffer().length);
+					}
+				};
+
+				scope.stop = function(){
+					if(fileService.getAudioBuffer()!==undefined){
+						playService.stopPlaying();
+					}
+				}
+
+				scope.pauseResume = function(){
+					if(fileService.getAudioBuffer()!==undefined){
+						playService.pauseResume();
+					}
+				}
+			}
+		};
+	});
---
+++ app/scripts/services/playService.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('testApp')
+	.factory('playService', function (fileService) {
+		var curSource;
+		var sServObj = {};
+		var audioContext;
+		sServObj.isPlaying = false;
+
+		function initAudioContext() {
+			try {
+				window.AudioContext = window.AudioContext || window.webkitAudioContext;
+				audioContext = new AudioContext();
+			} catch (e) {
+				alert('Error loading the AudioContext (could mean your browser does not support the HTML5 webaudio API):' + e);
+			}
+		};
+
+		sServObj.playFromTo = function (sampleStart, endSample) {
+			//var cutWavBuff = this.extractRelPartOfWav(sampleStart, endSample);
+			if (sServObj.isPlaying) {
+				sServObj.isPlaying = false;
+				curSource.stop(0);
+			} else {
+
+				sServObj.isPlaying = true;
+				if (fileService.getAudioBuffer().length > 0) { // if wav file is not empty
+					sServObj.decodeAndPlay(sampleStart, endSample);
+				}
+			}
+
+		};
+
+		sServObj.decodeAndPlay = function (sampleStart, endSample) {
+			if (typeof(audioContext) === 'undefined') {
+				initAudioContext();
+			}
+			var audioBuffer = fileService.getAudioBuffer();
+			if(audioBuffer !== undefined){
+					var startTime = sampleStart / audioBuffer.sampleRate;
+		            var endTime = endSample / audioBuffer.sampleRate;
+					var durTime = endTime - startTime;
+					curSource = audioContext.createBufferSource();
+					curSource.buffer = audioBuffer;
+					curSource.connect(audioContext.destination);
+					curSource.start(0, startTime, durTime);
+					curSource.onended = function () {
+						sServObj.isPlaying = false;
+					};
+			}
+		
+		};
+
+		//Not working
+		sServObj.stopPlaying = function (){
+			curSource.stop();
+			sServObj.isPlaying = false;
+		}
+
+		sServObj.pauseResume = function() {
+			if(sServObj.isPlaying){
+				sServObj.isPlaying = false;
+				audioContext.suspend();
+			} else {
+				sServObj.isPlaying = true;
+				audioContext.resume();
+			}
+		}
+
+
+		return sServObj;
+
+});
